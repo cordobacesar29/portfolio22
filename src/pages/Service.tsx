e9284e9e2cda270e4f55b6fe0eac86703850c650
César Córdoba <cordobacesar29@gmail.com>
2022-07-13 16:21:20 -0300

    add new components

--- src/pages/Service.tsx
+++ src/pages/Service.tsx
@@ -1,20 +1,20 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Heading, Text } from "@chakra-ui/react";
 import { useWindowSize } from "../hooks/useWindowSize";
-
+import { WhatIDo } from "../components/WhatIDo";
+import { HowIWork } from "../components/HowIWork";
 
 export const Service = () => {
   const { width } = useWindowSize();
   return (
-    <Flex
-      w={width} maxWidth="1440px" 
-      justifyContent="space-between"
-      alignItems="center"
-      direction={width > 768 ? "row" : "column"}
-      bg=" linear-gradient(to bottom,rgba(255,255,255,0),#d3e9ef);"
-    >
-      <Flex>
-        <h1>SERVICES</h1>
-
+    <Flex>
+      <Flex 
+        direction="column"
+      >
+        <Heading fontWeight="bold" m="1rem" size={width > 768 ? "3xl" : "lg"} mb="3rem" alignSelf={"center"}>
+          Work with the top talent
+        </Heading>
+        <WhatIDo />
+        <HowIWork />
       </Flex>
     </Flex>
   );
